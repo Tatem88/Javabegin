e62142725f4e888f81125ee972176f850f741d31
Tatem88 <81513185+Tatem88@users.noreply.github.com>
2021-09-14 21:41:15 +0300

    Add files via upload

---
+++ lesson1/Main.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import static lesson1.HomeWorkApp.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+}
+
